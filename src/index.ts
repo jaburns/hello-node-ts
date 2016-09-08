9812e12dde94d4dc0c2e77b2de194d55e9882135
Jeremy Burns <j@jaburns.net>
2016-09-08 15:22:26 -0600

    Switch to ES6 output for async/await support.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import * as express from 'express';
+import {Request, Response} from 'express';
 
 let app = express();
 
-app.get('/', function(req: express.Request, res: express.Response): void {
-    res.send('Hello world!\n');
+async function slowResponse(): Promise<string> {
+    await delay(500);
+    return 'Hello world!';
+}
+
+function delay(time: number): Promise<void> {
+    return new Promise<void>(resolve => { setTimeout(resolve, time); });
+}
+
+app.get('/', function(req: Request, res: Response): void {
+    slowResponse().then(msg => {
+        res.send(msg);
+    });
 });
 
 app.listen(3000, function(): void {
