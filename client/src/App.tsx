9604c713c43085a0f0e037c22019f3ca91b03c65
Jeremy Burns <j@jaburns.net>
2016-09-28 15:44:29 -0600

    Toggling items on click

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { connect, MapDispatchToPropsObject } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import { TodosState } from './state';
 import * as actions from './actions';
@@ -9,7 +10,9 @@ interface StateProps {
 }
 
 interface DispatchProps {
-    addTodo(text: string): any
+    addTodo(text: string): any;
+    completeTodo(id: number): any;
+    deleteTodo(id: number): any;
 }
 
 type Props = StateProps & DispatchProps;
@@ -18,14 +21,18 @@ const mapStateToProps = (state: TodosState): StateProps => ({
     todos: state
 });
 
-const mapDispatchToProps: MapDispatchToPropsObject = {
-    addTodo: actions.addTodo
-};
+const mapDispatchToProps = (dispatch: any) =>
+    bindActionCreators(actions as any, dispatch);
 
 @connect<StateProps, DispatchProps, any>(mapStateToProps, mapDispatchToProps)
 export class App extends React.Component<Props, any> {
     render() {
-        let vals = this.props.todos.map(todo => <li>{todo.text}</li>);
+        let vals = this.props.todos.map(todo => <div>
+            <button onClick={() => this.props.deleteTodo(todo.id)}> X </button>
+            <li onClick={() => this.props.completeTodo(todo.id)}>
+                {todo.text + (todo.completed ? ' X': '')}
+            </li>
+        </div>);
 
         return (<div>
             <h1>Todo App</h1>
