fd7401d3dd4439b43d0cf50a050c0a8f8f78a2e8
Jeremy Burns <j@jaburns.net>
2016-09-28 16:02:21 -0600

    Removed typings, using npm-based definitions with TS 2.0

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-npm install --global typescript typings webpack
+npm install --global typescript webpack
 npm install
 npm link typescript  # Needed for webpack ts-loader to use typescript
 
-pushd client; typings install; popd
-pushd server; typings install; popd
