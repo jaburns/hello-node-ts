43c986b73fa72a707c516a1183fd1e60a0810224
Jeremy Burns <j@jaburns.net>
2016-10-04 16:12:48 -0600

    Persisting app state to localStorage

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,13 +2,23 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { Store, createStore } from 'redux';
 import { Router, Route, Link, browserHistory } from 'react-router';
+import * as throttle from 'lodash/throttle';
 
 import createProvider from './utils/provider';
 import reducer from './reducers';
+import { saveState, loadState } from './storage';
 import Todos from './Todos';
 
-const store = createStore(reducer);
+
+const store = createStore(reducer, loadState());
 const Provider = createProvider();
+
+store.subscribe(throttle(() => {
+    const state = store.getState();
+    if (typeof state !== 'undefined') {
+        saveState(state);
+    }
+}, 1000));
 
 const Hello = () =>
     <div>
--- client/src/storage.ts
+++ client/src/storage.ts
@@ -1,12 +1,23 @@
-export const loadState = () => {
+import { TodosState } from './state';
+
+export const loadState = (): TodosState | undefined => {
     try {
         const serializedState = localStorage.getItem('state');
         if (serializedState === null) {
             return undefined;
         }
-        return JSON.parse(serializedState);
+        return JSON.parse(serializedState) as TodosState;
     }
     catch (err) {
         return undefined;
+    }
+};
+
+export const saveState = (state: TodosState): void => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState);
+    }
+    catch (err) {
     }
 };
