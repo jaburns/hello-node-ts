9c30823d5d4923a3b3c086ce923430996223745b
Jeremy Burns <j@jaburns.net>
2016-10-05 13:11:16 -0600

    Broke up main app component in to subcomponents

--- client/src/Todos.tsx
+++ client/src/Todos.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { ReactNode } from 'react';
 import { connect, MapDispatchToPropsObject } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router';
@@ -35,21 +36,6 @@ const mapDispatchToProps = {
     setVisibilityFilter
 };
 
-interface FilterProps {
-    onClick: () => void,
-    children?: React.ReactNode
-};
-
-const FilterLink = (props: FilterProps) =>
-    <a href="#"
-        onClick={e => {
-            e.preventDefault();
-            props.onClick();
-        }}
-    >
-        {props.children}
-    </a>;
-
 const getVisibleTodos = (todos: TodoItemState[], visibility: Visibility) :TodoItemState[] => {
     switch (visibility) {
         case 'SHOW_ALL':
@@ -61,25 +47,124 @@ const getVisibleTodos = (todos: TodoItemState[], visibility: Visibility) :TodoIt
     }
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)((props: StateProps & DispatchProps) => {
-    const visibleTodos = getVisibleTodos(props.todos, props.visibilityFilter);
+export default connect(mapStateToProps, mapDispatchToProps)((props: StateProps & DispatchProps) =>
+    <div>
+        <h1>Todo App</h1>
+        <p><Link to="/">Back to hello</Link></p>
 
-    const vals = visibleTodos.map(todo => <div>
-        <button onClick={() => props.deleteTodo(todo.id)}> X </button>
-        <li onClick={() => props.completeTodo(todo.id)}>
-            {todo.text + (todo.completed ? ' X': '')}
-        </li>
-    </div>);
+        <AddTodo onAddClick={props.addTodo} />
+        <TodoList todos={getVisibleTodos(props.todos, props.visibilityFilter)} onTodoClick={props.completeTodo} />
+        <Footer visibilityFilter={props.visibilityFilter} onFilterClick={props.setVisibilityFilter} />
+    </div>
+);
+
+// ------------------------------------------------------------------------------------
+
+interface FooterProps {
+    visibilityFilter: Visibility,
+    onFilterClick: (v:Visibility) => void
+};
+
+const Footer = (props: FooterProps) =>
+    <p>
+        Show:
+        {' '}
+        <FilterLink
+            filter="SHOW_ACTIVE"
+            currentFilter={props.visibilityFilter}
+            onClick={() => { props.onFilterClick('SHOW_ACTIVE'); }}>
+                Active
+        </FilterLink>
+        {', '}
+        <FilterLink
+            filter="SHOW_COMPLETED"
+            currentFilter={props.visibilityFilter}
+            onClick={() => { props.onFilterClick('SHOW_COMPLETED'); }}>
+                Completed
+        </FilterLink>
+        {', '}
+        <FilterLink
+            filter="SHOW_ALL"
+            currentFilter={props.visibilityFilter}
+            onClick={() => { props.onFilterClick('SHOW_ALL'); }}>
+                All
+        </FilterLink>
+    </p>
+
+// ------------------------------------------------------------------------------------
+
+interface FilterLinkProps {
+    filter: Visibility,
+    currentFilter: Visibility,
+    children?: ReactNode,
+    onClick: () => void
+};
+
+const FilterLink = (props: FilterLinkProps) => {
+    if (props.filter === props.currentFilter) {
+        return <span>{props.children}</span>
+    }
+    return <a
+        href="#"
+        onClick={e => {
+            e.preventDefault();
+            props.onClick();
+        }}>
+            {props.children}
+    </a>
+};
+
+// ------------------------------------------------------------------------------------
+
+interface AddTodoProps {
+    onAddClick: (text:String) => void;
+}
+
+const AddTodo = (props: AddTodoProps) => {
+    let inputElem: HTMLInputElement;
 
     return <div>
-        <h1>Todo App</h1>
-        <Link to="/">Back to hello</Link>
-        <button onClick={() => { props.addTodo('new TODO'); }}>
-            Add
+        <input ref={x => { inputElem = x; }} />
+        <button
+            onClick={() => {
+                props.onAddClick(inputElem.value);
+                inputElem.value = '';
+            }}>
+                Add Todo
         </button>
-        <FilterLink onClick={() => { props.setVisibilityFilter('SHOW_ACTIVE'); }}>Active</FilterLink>
-        <FilterLink onClick={() => { props.setVisibilityFilter('SHOW_ALL'); }}>All</FilterLink>
-        <FilterLink onClick={() => { props.setVisibilityFilter('SHOW_COMPLETED'); }}>Completed</FilterLink>
-        {vals}
     </div>;
-});
+};
+
+// ------------------------------------------------------------------------------------
+
+interface TodoProps {
+    onClick: () => void;
+    completed: boolean;
+    text: string;
+}
+
+const Todo = (props: TodoProps) =>
+    <li
+        onClick={props.onClick}
+        style={{ textDecoration: props.completed ? 'line-through' : 'none' }}>
+            {props.text}
+    </li>;
+
+// ------------------------------------------------------------------------------------
+
+interface TodoListProps {
+    todos: TodoItemState[];
+    onTodoClick: (id:string) => void;
+}
+
+const TodoList = (props: TodoListProps) =>
+    <ul>
+        {props.todos.map(todo =>
+            <Todo
+                key={todo.id}
+                {...todo}
+                onClick={() => props.onTodoClick(todo.id)} />
+        )}
+    </ul>;
+
+// ------------------------------------------------------------------------------------
